8a2beeba5206b92139ecc827f7b0a4faf90e142d
Mehdi <mahdi0131rain@gmail.com>
2021-12-02 22:02:21 +0330

    config vue

--- app/src/main.js
+++ app/src/main.js
@@ -3,13 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { loadFonts } from './plugins/webfontloader'
-import { Quasar } from 'quasar'
-import quasarUserOptions from './quasar-user-options'
 
 loadFonts()
 
-createApp(App)
-  .use(Quasar, quasarUserOptions)
-  .use(router)
-  .use(store)
-  .mount('#app')
+createApp(App).use(router).use(store).mount('#app')
