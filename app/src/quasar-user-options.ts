d173010fcebd2e97dd6a4c0b9cc82f5224ec8160
Mehdi <mahdi0131rain@gmail.com>
2021-12-05 21:36:20 +0330

    add quasar

---
+++ app/src/quasar-user-options.ts
@@ -0,0 +1,13 @@
+import "./styles/quasar.scss";
+import iconSet from "quasar/icon-set/ionicons-v4.js";
+import lang from "quasar/lang/fa-IR.js";
+import "@quasar/extras/roboto-font/roboto-font.css";
+import "@quasar/extras/ionicons-v4/ionicons-v4.css";
+
+// To be used on app.use(Quasar, { ... })
+export default {
+  config: {},
+  plugins: {},
+  lang: lang,
+  iconSet: iconSet,
+};
