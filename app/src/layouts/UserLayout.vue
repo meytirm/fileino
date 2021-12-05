e5236627e86341da9f9effd0c2c74ab64c6dbe47
Mehdi <mahdi0131rain@gmail.com>
2021-12-05 23:04:33 +0330

    config vue

---
+++ app/src/layouts/UserLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>User</div>
+</template>
+
+<script>
+export default {
+  name: 'UserLayout',
+}
+</script>
+
+<style scoped></style>
---
+++ app/src/layouts/WebsiteLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img
+              alt=""
+              src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg"
+            />
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <slot />
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img
+              alt=""
+              src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg"
+            />
+          </q-avatar>
+          <div>Title</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'WebsiteLayout.vue',
+}
+</script>
+
+<style scoped></style>
