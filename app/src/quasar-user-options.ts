ffd43be857ad885b188f09a590003065d067e76d
Mehdi <mahdi0131rain@gmail.com>
2021-12-10 19:36:31 +0330

    add notify

--- app/src/quasar-user-options.ts
+++ app/src/quasar-user-options.ts
@@ -1,13 +1,18 @@
-import "./styles/quasar.scss";
-import iconSet from "quasar/icon-set/ionicons-v4.js";
-import lang from "quasar/lang/fa-IR.js";
-import "@quasar/extras/roboto-font/roboto-font.css";
-import "@quasar/extras/ionicons-v4/ionicons-v4.css";
+import { Notify } from 'quasar'
+import './styles/quasar.scss'
+import iconSet from 'quasar/icon-set/ionicons-v4.js'
+import lang from 'quasar/lang/fa-IR.js'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/ionicons-v4/ionicons-v4.css'
 
 // To be used on app.use(Quasar, { ... })
 export default {
-  config: {},
-  plugins: {},
+  config: {
+    notify: {},
+  },
+  plugins: {
+    Notify,
+  },
   lang: lang,
   iconSet: iconSet,
-};
+}
