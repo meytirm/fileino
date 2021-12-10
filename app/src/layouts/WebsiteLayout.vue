7a25c089ad2b3cb7a5255ac67d71d2a7866790ee
Mehdi <mahdi0131rain@gmail.com>
2021-12-10 19:34:37 +0330

    add tooltip links

--- app/src/layouts/WebsiteLayout.vue
+++ app/src/layouts/WebsiteLayout.vue
@@ -2,15 +2,20 @@
   <q-layout view="hHh lpR fFf">
     <q-header elevated class="bg-primary text-white">
       <q-toolbar>
-        <q-toolbar-title>
+        <q-toolbar-title class="row">
           <q-avatar>
-            <img
-              alt=""
-              src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg"
-            />
+            <img alt="" src="../assets/logo.png" />
           </q-avatar>
-          Title
+          <span class="q-mr-md">فایلینو</span>
+          <template v-for="link in links" :key="link.label" class="row">
+            <q-separator dark vertical />
+            <q-btn stretch flat :label="link.label" />
+          </template>
         </q-toolbar-title>
+        <q-separator dark vertical />
+        <q-btn stretch flat label="ورود" />
+        <q-separator dark vertical />
+        <q-btn stretch flat label="ثبت نام" />
       </q-toolbar>
     </q-header>
     <q-page-container>
@@ -21,12 +26,9 @@
       <q-toolbar>
         <q-toolbar-title>
           <q-avatar>
-            <img
-              alt=""
-              src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg"
-            />
+            <img alt="" src="../assets/logo.png" />
           </q-avatar>
-          <div>Title</div>
+          فایلینیو
         </q-toolbar-title>
       </q-toolbar>
     </q-footer>
@@ -36,6 +38,17 @@
 <script lang="ts">
 export default {
   name: 'WebsiteLayout.vue',
+  setup() {
+    const links = [
+      { name: 'Newest', label: 'جدیدترین' },
+      { name: 'Javascript', label: 'جاوااسکریپت' },
+      { name: 'Laravel', label: 'لاراول' },
+    ]
+
+    return {
+      links,
+    }
+  },
 }
 </script>
 
